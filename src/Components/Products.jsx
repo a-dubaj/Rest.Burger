300bf73f3388b51d10b0c19f0b484962ef13bfb6
Andrzej Dubaj <dubaj.andrzej@gmail.com>
2021-06-02 15:50:05 +0200

    Update

--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Productbox from './Productbox'
 
 function Products() {
     return(
-        <div>
+        <div id='products'>
+            <h1>CHOOSE & ENJOY</h1>
+            <p>Lorem ipsum dolor </p>
+            <div className='a-cointainer'>
+                
+
+            </div>
         </div>
     )
 }
